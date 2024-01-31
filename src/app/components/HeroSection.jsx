63c0ce61a7c0626ca8a40c7f3e5a6cf9b4d4961d
DARKHUBRO <143330870+DARKHUBRO@users.noreply.github.com>
2024-01-31 12:30:28 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,8 +47,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            I am Professional Carder
           </p>
           <div>
             <Link href="#contact">
